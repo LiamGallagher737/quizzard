efd3f55c90574dc0fcb0f150062318765d8fd00b
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-09-30 15:36:01 +1300

    Custom error

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
 pub use quizzard_derive::*;
+use thiserror::Error;
 
 pub trait SelectEnum: Sized + 'static {
     const VARIANTS: &'static [Self];
     fn prompt(&self) -> &'static str;
     fn to_index(&self) -> usize;
     fn from_index(n: usize) -> Option<Self>;
+type Result<V> = std::result::Result<V, Error>;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("An IO error occurred")]
+    Io(#[from] std::io::Error),
+    #[error("Error: {0}")]
+    Other(&'static str),
 }
