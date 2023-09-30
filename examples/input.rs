88f5ae7e15fd30eda7b0f14da22bb9cae16336eb
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-09-30 20:22:57 +1300

    Reorder input example

--- examples/input.rs
+++ examples/input.rs
@@ -5,13 +5,13 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     let term = Term::stdout();
 
-    let answer = Text::new("What's your name?").ask(&term)?;
-    println!("You answered {answer:?}");
+    let name = Text::new("What's your name?").ask(&term)?;
 
-    let answer = Text::new("What's your job?")
+    let job = Text::new("What's your job title?")
         .default("Developer")
         .ask(&term)?;
-    println!("You answered {answer:?}");
+
+    println!("Your name is {name} and your job title is {job}");
 
     Ok(())
 }
