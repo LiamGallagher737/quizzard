a3e735d84959df7e18b106a3733aa8dcf032b500
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-09-30 15:36:05 +1300

    Custom error

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 pub use quizzard_derive::*;
 use thiserror::Error;
 
-pub trait SelectEnum: Sized + 'static {
-    const VARIANTS: &'static [Self];
-    fn prompt(&self) -> &'static str;
-    fn to_index(&self) -> usize;
-    fn from_index(n: usize) -> Option<Self>;
 type Result<V> = std::result::Result<V, Error>;
 
 #[derive(Error, Debug)]
